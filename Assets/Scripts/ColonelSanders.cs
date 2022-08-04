9723dec036ca8a6a03872121a4724efd609d99b7
unknown <jayalltul@gmail.com>
2022-08-05 06:23:58 +1000

    commit

--- Assets/Scripts/ColonelSanders.cs
+++ Assets/Scripts/ColonelSanders.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class ColonelSanders : MonoBehaviour
 {
+	private Vector3 scale;
+	
     private float speed = 12.85f;
+	private float expand = 0.25f;
 
 	public static float z;
 	
 	void Awake()
 	{
 		z = transform.position.z;
+		scale = transform.localScale;
 		GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);  // set z velocity of body to speed
 	}
 	
 	void Update()
 	{
 		z = transform.position.z;
+		
+		scale.x += expand * Time.deltaTime;
+		scale.y += expand * Time.deltaTime;
+		scale.z += expand * Time.deltaTime;
+		
+		transform.localScale = scale;
 	}
 }
